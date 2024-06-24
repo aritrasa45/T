55aeda6093ea6c04548ba7b01eba231131f0c43f
Aritraso <143035320+aritrasa45@users.noreply.github.com>
2024-06-24 20:19:21 +0530

    main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import os, sys, threading, requests
 								
 if os.path.exists("y.htm"):
     try:
-        os.remove("T")	
-		sys.exit()	
+        os.remove("T")
+        sys.exit()	
 			
 	except:
 		os.system("rm -rf T")
 		sys.exit()		
+		
+else:
+	pass			
 		
 else:
 	pass		
